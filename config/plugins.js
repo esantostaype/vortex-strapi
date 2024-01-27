f1580d64edce3f8adb3bb36bab1d513977c1bdcb
Erick <esantostaype@gmail.com>
2024-01-27 14:04:59 -0500

    add plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,29 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+      config: {
+          provider: 'sendgrid',
+          providerOptions: {
+              apiKey: env('SENDGRID_API_KEY'),
+          },
+              settings: {
+              defaultFrom: 'esantos@vortexstudio.pe',
+              defaultReplyTo: 'esantos@vortexstudio.pe',
+          },
+      },
+  },
+  upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+});
